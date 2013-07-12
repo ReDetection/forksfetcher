1e695c71dd0064ce2c4d363e91222860f5c400e4
ReDetection <redetection@gmail.com>
2013-07-13 00:49:42 +0700

    a little cleanup

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject forksfetcher "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Easiest way to look through all forks"
+  :url "https://github.com/ReDetection/forksfetcher"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"],
