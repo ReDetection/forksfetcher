764ce99bed47ba9c9396da6ea0402294e3e4d1db
ReDetection <redetection@gmail.com>
2013-07-24 20:34:55 +0700

    little cleanup: let instead of def

--- src/forksfetcher/core.clj
+++ src/forksfetcher/core.clj
@@ -5,20 +5,17 @@
 
 (defn remove-from-end [s end]
   (if (.endsWith s end)
-      (.substring s 0 (- (count s)
-                         (count end)))
-    s))
+      (.substring s 0 (- (count s) (count end)))
+      s))
 
 (defn githubHtmlUrl [gitrepo]
-  (def conf (.getConfig (.getRepository gitrepo)))
-  (def origin (first (.getSubsections conf "remote")))
-  (remove-from-end (.getString conf "remote" origin "url") ".git")
-)
+  (let [conf (.getConfig (.getRepository gitrepo))]
+    (let [origin (first (.getSubsections conf "remote"))]
+      (remove-from-end (.getString conf "remote" origin "url") ".git"))))
 
 (defn namesFromUrl [githubURL]
-  (def parts (drop 3 (split githubURL #"/")))
-  [(first parts) (first (rest parts))]
-)
+  (let [parts (drop 3 (split githubURL #"/"))]
+    [(first parts) (first (rest parts))]))
 
 (defn getAllPagesWForks [repourl]
   (let [[user reponame] (namesFromUrl repourl)]
@@ -29,13 +26,7 @@
           (do (println "Rate limit exceeded, I will break") [result 1])
           (if (< 0 (count forkspage))
             (recur (concat result forkspage) (+ 1 page))
-            [result 0]
-          )
-        )
-      )
-    )
-  )
-)
+            [result 0]))))))
 
 (defn getForks [repourl]
   (loop [giturls nil
@@ -44,22 +35,13 @@
       (let [[forksInfo limitWasReached] (getAllPagesWForks (first htmlurls))]
         (if (= 1 limitWasReached)
           giturls
-          (do 
-            (def gitsadd (map :clone_url forksInfo))
-            (def htmlsadd (filter identity (map (fn ([x] (if (> (:forks_count x) 0) (:html_url x)))) forksInfo)))
-            (recur (concat giturls gitsadd)
-                   (concat (rest htmlurls) htmlsadd))
-          )
-        )
-      )
-      giturls
-    )
-  )  
-)
+          (recur (concat giturls (map :clone_url forksInfo))
+                 (concat (rest htmlurls) (filter identity (map (fn ([x] (if (> (:forks_count x) 0) (:html_url x)))) forksInfo))))))
+      giturls)))
 
 (defn addRemoteAndFetch [gitrepo url]
-  (let [[user reponame] (namesFromUrl url)]
-    (def conf (.getConfig (.getRepository gitrepo)))
+  (let [[user reponame] (namesFromUrl url)
+        conf (.getConfig (.getRepository gitrepo))]
     (.setString conf "remote" user "url" url)
     (.setString conf "remote" user "fetch" (str "+refs/heads/*:refs/remotes/" user "/*"))
     (.save conf)
@@ -68,26 +50,20 @@
       (catch org.eclipse.jgit.api.errors.TransportException e 
         (println (str "Nothing to fetch from " user "'s fork")))
       (catch java.lang.Exception e 
-        (println (str "Can't fetch " user "'s fork — please check availability at url " (remove-from-end url ".git"))))
-    )
-  )
-)
+        (println (str "Can't fetch " user "'s fork — please check availability at url " (remove-from-end url ".git")))))))
 
 (defn addForks [gitrepo forks]
   (dorun (for [fork forks]
-    (addRemoteAndFetch gitrepo fork)
-  ))
-)
+    (addRemoteAndFetch gitrepo fork))))
 
 (defn -main 
   "The application's main function"
   ([& args]
-    (def repo-dir (if args (first args) (System/getProperty "user.dir")))
-    (if-let [repo (try (git/load-repo repo-dir) (catch java.io.FileNotFoundException e nil) )]
-      (do 
-        (def repoURL (githubHtmlUrl repo))
-        (println (str "First remote url at " repo-dir " is " repoURL))
-        (def forks (getForks repoURL))
-        (println (str "It has " (count forks) " forks"))
-        (addForks repo forks))
-      "Can't find any git repo")))
+    (let [repo-dir (if args (first args) (System/getProperty "user.dir"))]
+      (if-let [repo (try (git/load-repo repo-dir) (catch java.io.FileNotFoundException e nil) )]
+        (let [repoURL (githubHtmlUrl repo)]
+          (println (str "First remote url at " repo-dir " is " repoURL))
+          (let [forks (getForks repoURL)]
+            (println "It has" (count forks) "forks")
+            (addForks repo forks)))
+        "Can't find any git repo"))))
